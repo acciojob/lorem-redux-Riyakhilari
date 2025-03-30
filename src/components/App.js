69845928ee373676e9b66c076246e0e68dd62d8f
Riya <149504570+Riyakhilari@users.noreply.github.com>
2025-03-30 15:51:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,19 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchData } from "./actions";
-import "./styles/App.css";
+
+
+import React from "react";
+import './../styles/App.css';
+import { Provider } from "react-redux";
+import store from "./store";
+import Lorem from "./Lorem";
 
 const App = () => {
-  const dispatch = useDispatch();
-  const { loading, data, error } = useSelector((state) => state);
-
-  useEffect(() => {
-    dispatch(fetchData());
-  }, [dispatch]);
-
   return (
     <div>
-      <h1>Lorem Redux</h1>
-      {loading && <p>Loading...</p>}
-      {error && <p>Error: {error}</p>}
-      {data.map((item, index) => (
-        <div key={index} className="card">
-          <h2>{item.title}</h2>
-          <p>{item.body}</p>
-        </div>
-      ))}
+     <Provider store={store}>
+      <Lorem />
+    </Provider>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App

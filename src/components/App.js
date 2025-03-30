79d6113ad80474dab9c5640d6ed1738bef269441
Riya <149504570+Riyakhilari@users.noreply.github.com>
2025-03-30 15:41:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchData } from "./actions";
+import "./styles/App.css";
 
 const App = () => {
+  const dispatch = useDispatch();
+  const { loading, data, error } = useSelector((state) => state);
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Lorem Redux</h1>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      {data.map((item, index) => (
+        <div key={index} className="card">
+          <h2>{item.title}</h2>
+          <p>{item.body}</p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

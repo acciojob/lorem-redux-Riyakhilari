4baeb8471adbd55307b566adfc67e1456fed0e3f
Riya <149504570+Riyakhilari@users.noreply.github.com>
2025-03-30 16:23:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
 
+
+
+import React from "react";
+import './../styles/App.css';
+import { Provider } from "react-redux";
+import store from "./store";
+import Lorem from "./Lorem";
+
+const App = () => {
+  return (
+    <div>
+     <Provider store={store}>
+      <Lorem />
+    </Provider>
+    </div>
+  )
+}
+
+export default App
+
